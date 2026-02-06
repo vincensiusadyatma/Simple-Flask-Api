52fea3406ccc88d7fdbc6d2f83805e17484492ac
vincenciusadyatma <adyatmav@gmail.com>
2026-02-06 19:04:44 +0700

    feat: create seeder for products data

--- src/__init__.py
+++ src/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from .config import Config
 from .routes import auth_bp
+from .cli.product_seed_cli import product_seed
 
 def createApp():
     app = Flask(__name__)
     app.config.from_object(Config)
+    app.cli.add_command(product_seed)
+    
     app.register_blueprint(auth_bp)
     return app
